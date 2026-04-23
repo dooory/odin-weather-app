e7602d4df565fcc751f008637249c9748d1f1934
dooory <78720364+dooory@users.noreply.github.com>
2026-04-23 12:32:50 +1200

    feat: create getLocationWeather

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import "./style.css";
 import WeatherFetcher from "./weatherFetcher";
 
 WeatherFetcher.getLocationWeather("New York", "metric").then((data) => {
-	console.log(data);
+	console.log(data.current);
 });
--- src/weatherFetcher.js
+++ src/weatherFetcher.js
@@ -3,7 +3,7 @@ const apiKey = "MUKZGWAK2SR7CVH3SAVSB8KQ4";
 const weatherTimelineURL =
 	"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline";
 
-const getLocationWeather = async (location, unitGroup) => {
+const fetchLocationWeather = async (location, unitGroup) => {
 	let url = `${weatherTimelineURL}/${location}?key=${apiKey}&unitGroup=${unitGroup}&contentType=json&include=current`;
 
 	try {
@@ -16,6 +16,25 @@ const getLocationWeather = async (location, unitGroup) => {
 		}
 
 		throw new Error(response.status);
+	} catch (err) {
+		return err.message;
+	}
+};
+
+const getLocationWeather = async (location, unitGroup) => {
+	try {
+		const rawData = await fetchLocationWeather(location, unitGroup);
+		const weather = await {
+			current: {
+				feelslike: rawData.currentConditions.feelslike,
+				temp: rawData.currentConditions.temp,
+				conditions: rawData.currentConditions.conditions,
+				icon: rawData.currentConditions.icon,
+				location: rawData.address,
+			},
+		};
+
+		return weather;
 	} catch (err) {
 		return err.message;
 	}
